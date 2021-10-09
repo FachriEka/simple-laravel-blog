a84a4a2165792807cbd7390d53490f05a6932c82
ICHWAN RIZKY <78690451+irizkyw@users.noreply.github.com>
2021-10-09 16:42:52 +0000

    update views

--- resources/views/layouts/includes/_sidebar.blade.php
+++ resources/views/layouts/includes/_sidebar.blade.php
@@ -14,14 +14,14 @@
           <a href="about" class="nav-link w-nav-link">Tentang Kami</a>
           <a href="contact" class="nav-link w-nav-link">Kategori</a>
           <div class="divider"></div>
-          // <div class="social-link-group">
-          //   <a href="#" class="social-icon-link w-inline-block">
-          //     <img src="https://assets.website-files.com/5e4b19aa01d14d49bd4eabde/5e4b19aa01d14d6e5e4eac14_social-03-white.svg" width="25" alt="" />
-          //   </a>
-          //   <a href="#" class="social-icon-link w-inline-block">
-          //     <img src="https://assets.website-files.com/5e4b19aa01d14d49bd4eabde/5e4b19aa01d14ddb354eac82_social-07-white.svg" width="25" alt="" />
-          //   </a>
-          // </div>
+          <div class="social-link-group">
+            <a href="#" class="social-icon-link w-inline-block">
+              <img src="https://assets.website-files.com/5e4b19aa01d14d49bd4eabde/5e4b19aa01d14d6e5e4eac14_social-03-white.svg" width="25" alt="" />
+            </a>
+            <a href="#" class="social-icon-link w-inline-block">
+              <img src="https://assets.website-files.com/5e4b19aa01d14d49bd4eabde/5e4b19aa01d14ddb354eac82_social-07-white.svg" width="25" alt="" />
+            </a>
+          </div>
         </nav>
         <div class="menu-button w-nav-button">
           <div class="w-icon-nav-menu"></div>
