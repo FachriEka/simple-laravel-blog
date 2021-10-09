d6f99641365f3d59cc64c94998ce9e45b6edf702
Fachri Eka <71977365+FachriEka@users.noreply.github.com>
2021-10-09 17:38:52 +0000

    fix seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-use Faker\Generator as Faker;
 
 class UserFactory extends Factory
 {
@@ -21,11 +20,11 @@ class UserFactory extends Factory
      *
      * @return array
      */
-    public function definition(Faker $faker)
+    public function definition()
     {
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
